61def3c2543c0819ff22ebf50af6b04d960ebe39
Daniel Rabban <rabban.daniel@gmail.com>
2018-05-06 20:34:09 +0300

    Project finished

---
+++ script.js
@@ -0,0 +1,80 @@
+$( document ).ready(function() {
+
+    function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
+
+    $("#btnSubmit").click(function() {
+
+        var errorMessage = "";
+
+        var fieldsMissing = "";
+
+        if ($("#email").val() == "") {
+
+            fieldsMissing += "<br>Email";
+        }
+
+        if ($("#tel").val() == "") {
+
+            fieldsMissing += "<br>Telephone";
+        }
+
+        if ($("#password").val() == "") {
+
+            fieldsMissing += "<br>Password";
+        }
+
+        if ($("#passwordConfirm").val() == "") {
+
+            fieldsMissing += "<br>Confirm Password";
+        }
+
+        if (fieldsMissing != "") {
+
+            errorMessage += "<p>The following field(s) are missing!</p>" + fieldsMissing;
+        }
+
+        if (isEmail($("#email").val()) == false) {
+            
+            errorMessage += "<p>Your email address is not valid</p>";
+
+        }
+        
+        if ($.isNumeric($("#tel").val()) == false) {
+
+            errorMessage += "<p>Your phone number is not numeric</p>";
+
+        }
+
+        if ($("#password").val() != $("#passwordConfirm").val()) {
+
+            errorMessage += "<p>Your passwords don't match</p>";
+
+        }
+
+        if (errorMessage != "") {
+
+            $("#errorMessage").html(errorMessage);
+
+        } else {
+
+            $("#successMessage").show();
+            $("#errorMessage").hide();
+
+        }
+
+    });
+
+
+
+
+
+
+    
+
+
+
+
+});
